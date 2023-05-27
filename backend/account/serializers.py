5137c7b8592a9e4805ef323448856ab532f04fcb
HamzaKhan001 <spaceus9@gmail.com>
2023-05-28 01:19:42 +0500

    accounts app serializer

---
+++ backend/account/serializers.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+
+
+class SignUpSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=User
+        fields=['first_name','last_name','email','password']
+        
+        
+        
