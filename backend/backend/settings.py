bb2acbc572070f1572f469150dc0562a2a4bcac4
HamzaKhan001 <spaceus9@gmail.com>
2023-05-27 11:38:27 +0500

    db added

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': "jobs-api",
+        'USER':'postgres',
+        'PASSWORD':'postgres',
+        'HOST':'localhost',
+        'PORT':'5432'
+        
     }
 }
 
