c55c1f64662a7fe534e05d9c8767de19a06a56d7
HamzaKhan001 <spaceus9@gmail.com>
2023-05-27 12:49:45 +0500

    added Jobtype models

--- backend/job/models.py
+++ backend/job/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+
+class JobType(models.TextChoices):
+    Permanent= 'Permanent'
+    Temporary= 'Temporary'
+    Internship = 'Internship'
+    
+
+class Job(models.Model):
+    title=models.CharField(max_length=200,null=True)
+    description=models.TextField(null=True)
+    email= models.EmailField(null=True)
+    address= models.CharField(max_length=100,null=True)
+    jobType=models.CharField(max_length=10, choices= JobType.choices, default= JobType.Permanent)
