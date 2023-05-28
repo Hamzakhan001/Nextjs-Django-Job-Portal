924c404f4f48b0a451fcd38c4d0454d4f14da43e
HamzaKhan001 <spaceus9@gmail.com>
2023-05-28 17:33:33 +0500

    job applied view

--- backend/job/views.py
+++ backend/job/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view,permission_classes
 from .models import Job,CandidatesApplied
-from .serializers import JobSerializer
+from .serializers import JobSerializer,CandidatesAppliedSerializer
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
 from rest_framework import status
@@ -138,3 +138,14 @@ def applyToJob(request,pk):
         'applied':True,
         'job_id':jobApplied.id
     },status=status.HTTP_200_OK)
+    
+    
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def appliedJobs(request):
+    args={'user_id':request.user.id}
+    jobs= CandidatesApplied.objects.filter(**args)
+    
+    serializer=CandidatesAppliedSerializer(jobs,many=True)
+    
+    return Response(serializer.data)
